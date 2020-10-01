1a5e45f081d37bd071a32f25958c25e7150d1d72
baskar.appsdev <baskar.appsdev@gmail.com>
2020-10-01 15:21:36 -0500

    IRepository and DBTables

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using Infrastructure.Data;
+using Core.Interfaces;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -25,7 +26,9 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+             services.AddScoped<IProductRepository, ProductRepository>();
+
+             services.AddControllers();
 
              services.AddDbContext<StoreContext>(x =>
             {
